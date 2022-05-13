41ca67fbdc2d396f3e490998b1ff2456b1a4e004
Indra Santosa <indrasantosa90@gmail.com>
2022-05-13 14:36:29 +0800

    Update reference to DV Ditto

--- src/docs/pages/DashboardPage.tsx
+++ src/docs/pages/DashboardPage.tsx
@@ -10,7 +10,7 @@ const DashboardPage: FC = () => {
   return (
     <div className="mx-auto max-w-screen-xl p-4 lg:p-12 lg:text-center">
       <h2 className="mb-4 text-2xl font-bold text-gray-900 dark:text-white lg:text-center lg:text-4xl lg:font-extrabold lg:leading-snug 2xl:px-48">
-        Tailwind CSS Components
+        DV Ditto Components
       </h2>
       <p className="mb-10 text-lg font-normal text-gray-500 dark:text-gray-400 lg:mb-16 lg:text-center lg:text-xl xl:px-64">
         Explore the whole collection of open-source web components and interactive elements built with the utility
