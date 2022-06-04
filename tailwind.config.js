3df21dd668d4de44ecf7b503d18e71d76bd5bf33
Indra Santosa <indrasantosa90@gmail.com>
2022-06-04 10:56:54 +0800

    Add color page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mono: {
+          0: '#ffffff',
+          10: '#f7f7f8',
+          20: '#eeeeee',
+          30: '#d6d6d6',
+          40: '#c8c8c8',
+          60: '#969696',
+          80: '#444444',
+          100: '#000000',
+        },
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
