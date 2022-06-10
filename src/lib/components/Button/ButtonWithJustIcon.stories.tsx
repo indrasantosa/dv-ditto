63c4d848927dd5d4a68870366333a07209903a84
Indra Santosa <indrasantosa90@gmail.com>
2022-06-10 15:41:39 +0800

    Add With just icon button

---
+++ src/lib/components/Button/ButtonWithJustIcon.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { withDesign } from 'storybook-addon-designs';
+import { BiCheck } from 'react-icons/bi';
+
+import { Button, ButtonComponentProps } from '.';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  decorators: [withDesign],
+} as Meta;
+
+const Template: Story<ButtonComponentProps> = (args) => <Button className="!w-24" {...args} />;
+
+export const ButtonWithJustIcon = Template.bind({});
+ButtonWithJustIcon.storyName = 'With Just Icon';
+ButtonWithJustIcon.args = {
+  color: 'primary',
+  size: 'sm',
+  icon: BiCheck,
+};
+
+ButtonWithJustIcon.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/Wq4MDMgwh9N1xDXIsM0tx2/DS-LT?node-id=50763%3A5035',
+  },
+};
