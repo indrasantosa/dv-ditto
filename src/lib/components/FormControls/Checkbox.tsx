2082b00d30fd617f715c299cd8d68105632338ae
Indra Santosa <indrasantosa90@gmail.com>
2022-06-12 19:54:27 +0800

    Update checkbox styles

--- src/lib/components/FormControls/Checkbox.tsx
+++ src/lib/components/FormControls/Checkbox.tsx
@@ -6,7 +6,7 @@ export type CheckboxProps = Omit<ComponentProps<'input'>, 'type'>;
 export const Checkbox: FC<CheckboxProps> = ({ className, ...props }) => (
   <input
     className={classNames(
-      'h-4 w-4 rounded border border-gray-300 bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600',
+      'h-6 w-6 rounded border border-primary-100 bg-functional-10 text-primary-100 hover:bg-primary-80 focus:ring-2 focus:ring-primary-80',
       className,
     )}
     type="checkbox"
