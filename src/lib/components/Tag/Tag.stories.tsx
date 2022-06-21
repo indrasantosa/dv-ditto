8a14d43c6b61d3cba1b2f38e58abfbb24becf5bb
Indra Santosa <indrasantosa90@gmail.com>
2022-06-22 00:32:55 +0800

    Add tag element

---
+++ src/lib/components/Tag/Tag.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { withDesign } from 'storybook-addon-designs';
+import { HiCheck } from 'react-icons/hi';
+
+import { Tag, TagProps } from '.';
+
+export default {
+  title: '02. Components/Tag',
+  component: Tag,
+  decorators: [withDesign],
+} as Meta;
+
+const Template: Story<TagProps> = (args) => (
+  <div className="flex items-center">
+    <Tag {...args} />
+  </div>
+);
+
+export const DefaultTag = Template.bind({});
+DefaultTag.storyName = 'Default';
+DefaultTag.args = {
+  size: 'sm',
+  children: 'Default',
+};
+DefaultTag.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/cneAdsZ0oyaJmzoTqzedEV/Ditto-Design-System-(SIN-XD)?node-id=51005%3A17102',
+  },
+};
+
+export const TagWithIcon = Template.bind({});
+TagWithIcon.storyName = 'Tag with icon';
+TagWithIcon.args = {
+  color: 'category1',
+  icon: HiCheck,
+  size: 'sm',
+  children: '2 minutes ago',
+};
+TagWithIcon.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/cneAdsZ0oyaJmzoTqzedEV/Ditto-Design-System-(SIN-XD)?node-id=51005%3A17102',
+  },
+};
+
+export const TagOnlyIcon = Template.bind({});
+TagOnlyIcon.storyName = 'Tag only icon';
+TagOnlyIcon.args = {
+  color: 'category1',
+  size: 'sm',
+  icon: HiCheck,
+};
+TagOnlyIcon.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/cneAdsZ0oyaJmzoTqzedEV/Ditto-Design-System-(SIN-XD)?node-id=51005%3A17102',
+  },
+};
---
+++ src/lib/components/Tag/index.tsx
@@ -0,0 +1,52 @@
+import { ComponentProps, FC, PropsWithChildren } from 'react';
+import classNames from 'classnames';
+
+export type TagColor = 'category1' | 'category2' | 'category3' | 'success' | 'error' | 'info' | 'warning';
+
+export type TagProps = PropsWithChildren<{
+  color?: TagColor;
+  size?: 'sm' | 'md';
+  href?: string;
+  icon?: FC<ComponentProps<'svg'>>;
+}>;
+
+const colorClasses: Record<TagProps['color'] & string, string> = {
+  category1: /*tw*/ 'bg-primary-10 text-primary-100',
+  category2: /*tw*/ 'bg-secondary-10 text-secondary-100',
+  category3: /*tw*/ 'bg-tertiary-10 text-tertiary-100',
+  success: /*tw*/ 'bg-state-success text-background-white',
+  warning: /*tw*/ 'bg-state-warning text-background-white',
+  info: /*tw*/ 'bg-state-info text-background-white',
+  error: /*tw*/ 'bg-state-error text-background-white',
+};
+
+const sizeClasses: Record<TagProps['size'] & string, string> = {
+  sm: /*tw*/ 'text-xs rounded-full px-2 py-1 font-normal',
+  md: /*tw*/ 'text-sm leading-6 rounded-full px-4 py-1 font-bold',
+};
+
+const iconSizeClasses: Record<TagProps['size'] & string, string> = {
+  sm: 'w-3 h-3',
+  md: 'w-3.5 h-3.5',
+};
+
+export const Tag: FC<TagProps> = ({ children, color = 'category1', size = 'sm', href, icon: Icon }) => {
+  const span = (
+    <>
+      <span
+        className={classNames('flex h-fit items-center gap-1 font-semibold', colorClasses[color], sizeClasses[size])}
+      >
+        {Icon && <Icon className={classNames(iconSizeClasses[size])} />}
+        {children && <span>{children}</span>}
+      </span>
+    </>
+  );
+
+  return href ? (
+    <a className="group" href={href}>
+      {span}
+    </a>
+  ) : (
+    span
+  );
+};
